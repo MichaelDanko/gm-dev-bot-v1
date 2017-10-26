79480aa9e12149bfb330f4eb9c04ff6784cb538b
andrewgremlich <andrew.gremlich@gmail.com>
2017-10-25 22:03:07 -0500

    updated database seed just incase it is needed in server.

--- dbModules/seedDb.js
+++ dbModules/seedDb.js
@@ -1,39 +1,46 @@
 function seedDatabase(client) {
-/*NOT UPDATED*/
-  client.query(`
-          CREATE SEQUENCE IF NOT EXISTS USER_TO_QUEST_SEQ  INCREMENT;
-          CREATE SEQUENCE IF NOT EXISTS QUEST_TO_ANSWER_SEQ INCREMENT;
 
-          CREATE TABLE IF NOT EXISTS QUESTIONS
-          (
-              QUEST_ID    UUID   PRIMARY KEY      NOT NULL
-            , QUESTION    VARCHAR                 NOT NULL
-            , ENTITY      VARCHAR                 NOT NULL
-            , CREATED_ON  timestamp default current_timestamp NOT NULL
-          );
+  client.query(`DROP TABLE QUESTIONS CASCADE;
+                DROP TABLE ANSWERS CASCADE;
+                DROP TABLE USER_TO_QUESTION CASCADE;
+                DROP TABLE QUESTIONS_TO_ANSWERS CASCADE;
 
-          CREATE TABLE IF NOT EXISTS ANSWERS
-          (
-              ANSWER_ID UUID PRIMARY KEY    NOT NULL
-            , ANSWER    VARCHAR             NOT NULL
-            , CREATED_ON  timestamp default current_timestamp NOT NULL
-          );
+                DROP SEQUENCE USER_TO_QUEST_SEQ;
+                DROP SEQUENCE QUEST_TO_ANSWER_SEQ;
 
-          CREATE TABLE IF NOT EXISTS USER_TO_QUESTION
-          (
-              ID          SERIAL  PRIMARY KEY   DEFAULT NEXTVAL('USER_TO_QUEST_SEQ') NOT NULL
-            , QUEST_ID    UUID    REFERENCES QUESTIONS (QUEST_ID)  NOT NULL
-            , USER_ID     UUID    REFERENCES ANSWERS (ANSWER_ID)   NOT NULL
-            , CREATED_ON  timestamp default current_timestamp NOT NULL
-          );
+                CREATE SEQUENCE IF NOT EXISTS USER_TO_QUEST_SEQ START 1000 INCREMENT 1 NO MAXVALUE;
+                CREATE SEQUENCE IF NOT EXISTS QUEST_TO_ANSWER_SEQ START 1000 INCREMENT 1 NO MAXVALUE;
 
-          CREATE TABLE IF NOT EXISTS QUESTIONS_TO_ANSWERS
-          (
-              ID          SERIAL PRIMARY KEY   DEFAULT NEXTVAL('USER_TO_QUEST_SEQ') NOT NULL
-            , QUEST_ID    UUID   REFERENCES QUESTIONS (QUEST_ID)  NOT NULL
-            , ANSWER_ID   UUID   REFERENCES ANSWERS (ANSWER_ID)   NOT NULL
-            , CREATED_ON  timestamp default current_timestamp NOT NULL
-          );`
+                CREATE TABLE IF NOT EXISTS QUESTIONS
+                (
+                    QUEST_ID    UUID      PRIMARY KEY   UNIQUE      NOT NULL
+                    , QUESTION    VARCHAR                             NOT NULL
+                    , ENTITY      VARCHAR                             NOT NULL
+                    , CREATED_ON  timestamp default current_timestamp NOT NULL
+                  );
+
+                  CREATE TABLE IF NOT EXISTS ANSWERS
+                  (
+                    ANSWER_ID   UUID      PRIMARY KEY  UNIQUE       NOT NULL
+                    , ANSWER      VARCHAR                             NOT NULL
+                    , CREATED_ON  timestamp default current_timestamp NOT NULL
+                  );
+
+                  CREATE TABLE IF NOT EXISTS USER_TO_QUESTION
+                  (
+                    ID          SERIAL      PRIMARY KEY       UNIQUE          NOT NULL
+                    , QUEST_ID    UUID        REFERENCES QUESTIONS (QUEST_ID)   NOT NULL
+                    , USER_ID     VARCHAR                                       NOT NULL
+                    , CREATED_ON  timestamp default current_timestamp           NOT NULL
+                  );
+
+                  CREATE TABLE IF NOT EXISTS QUESTIONS_TO_ANSWERS
+                  (
+                    ID          SERIAL      PRIMARY KEY       UNIQUE          NOT NULL
+                    , QUEST_ID    UUID        REFERENCES QUESTIONS (QUEST_ID)   NOT NULL
+                    , ANSWER_ID   UUID        REFERENCES ANSWERS (ANSWER_ID)    NOT NULL
+                    , CREATED_ON  timestamp default current_timestamp           NOT NULL
+                  );`
       , (err, res) => {
           if (err) throw err
           console.log(res)
