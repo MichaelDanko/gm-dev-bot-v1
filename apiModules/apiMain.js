9d4a6e576f0155eca9d9c848074f6fc352acb4d5
andrewgremlich <andrew.gremlich@gmail.com>
2017-10-25 21:59:17 -0500

    Added /api/getQuestions/, and /api/helloWorld/ api end points

--- apiModules/apiMain.js
+++ apiModules/apiMain.js
@@ -1,8 +1,30 @@
 function main(server, client) {
+
   server.get('/api/getQuestions/', function(req, res, next) {
-    res.send(req.params);
-    return next();
-  });
+    client.query(`
+                  SELECT
+                      QUESTIONS.QUESTION
+                    , USER_TO_QUESTION.USER_ID
+                  FROM QUESTIONS
+                  LEFT JOIN
+                    USER_TO_QUESTION
+                  ON QUESTIONS.QUEST_ID = USER_TO_QUESTION.QUEST_ID;
+                `, (err, resp) => {
+                    if (err) throw err
+                    console.log(resp.rows)
+                    res.send(resp.rows)
+                    client.end()
+                  }
+    )
+
+    return next()
+  })
+
+  server.get('/api/helloWorld/', function(req, res, next) {
+    res.send('helloWorld')
+    return next()
+  })
+
 }
 
 module.exports = main
